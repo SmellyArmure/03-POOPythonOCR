9ab594f1a880aa2e5020793b13020657ee3e3c22
Maryse <muller.maryse@gmail.com>
2019-01-14 11:30:22 +0100

    ajouté le fichier agents-100k.json, créé une liste avec les données des agents (dictionnaire), puis une liste d'instances de classe Agent dotés d'une agréabilité, puis une liste des agréabilités.

--- World.py
+++ World.py
@@ -1,7 +1,27 @@
+import json
+
+#Classe Agent
 class Agent:
 	def __init__(self, agreeableness):
 		self.agreeableness=agreeableness
 
-first_agent = Agent(1.5)
+listAgent = []
+listAgreeableness = []
 
-print("l'agréabilité de l'agent est : " + str(first_agent.agreeableness))
+fichAgents = open("agents-100k.json")
+Data = json.load(fichAgents)
+
+for element in Data:
+	listAgent.append(Agent(element["agreeableness"]))
+	#listAgreeableness.append(element["agreeableness"])
+	listAgreeableness.append(listAgent[-1].agreeableness)
+
+fichAgents.close()
+
+somme = sum(listAgreeableness)
+total = len(listAgreeableness)
+moyenne = somme/total
+
+print(somme)
+print(total)
+print(moyenne)
