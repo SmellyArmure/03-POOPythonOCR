d37e95141fa34df61bddfbe10229a36fc88e112f
Maryse <muller.maryse@gmail.com>
2019-01-14 14:58:39 +0100

    condensé (enlevé des variables inutiles), mis deux étoiles dans la fonction __init__, et crée un fichier main pour plus de clarté.

--- World.py
+++ World.py
@@ -2,23 +2,20 @@ import json
 
 #Classe Agent
 class Agent:
-	def __init__(self, agAttributs):
+	def __init__(self, **agAttributs):
 		for nomAtt, valAtt in agAttributs.items():
 			setattr(self, nomAtt, valAtt)
 
 listAgents = []
 listAgreeableness = []
 
-fichAgents = open("agents-100k.json")
-data = json.load(fichAgents)
+def main():
+	for dico in json.load(open("agents-100k.json")):
+ 		listAgents.append(Agent(**dico))
+ 		listAgreeableness.append(listAgents[-1].agreeableness)
 
-for element in data:
- 	listAgents.append(Agent(element))
- 	listAgreeableness.append(listAgents[-1].agreeableness)
+main()
 
 monAgent = listAgents[2]
-
-fichAgents.close()
-
 print(str(monAgent.agreeableness))
 
