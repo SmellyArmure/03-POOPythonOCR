613730aa5bb7b597a73f2dbfeefc0fc038dce02b
Maryse <muller.maryse@gmail.com>
2019-01-14 17:22:34 +0100

    crée une classe Zone, contenant des attributs de classe et une méthode de classe init_Zones qui initialise le tableau de toutes les zones (att de classe aussi) de largeur et longueur 1 degré.

--- World.py
+++ World.py
@@ -1,5 +1,6 @@
 import json
 import math
+import random
 
 #Classe Agent
 class Agent:
@@ -16,25 +17,45 @@ class Position:
 
 	@property
 	def long_rad(self):
-		self._long_rad = self.longitude*math.pi/180
-		return self._lat_rad
+		return self.longitude*math.pi/180
 	
 	@property
 	def lat_rad(self):
-		self._lat_rad = self.latitude*math.pi/180
-		return self._lat_rad
+		return self.latitude*math.pi/180
+
+#Classe Zone
+class Zone:
+
+	ZONES=[]
+	MIN_LAT_DEG = -90
+	MAX_LAT_DEG = 90
+	MIN_LONG_DEG = -180
+	MAX_LONG_DEG = 180
+	WIDTH_DEG = 1
+	HEIGHT_DEG = 1
+
+	def __init__(self, coin1, coin2):
+		self.coin1 = coin1
+		self.coin2 = coin2
+
+	@classmethod
+	def init_zones(cls):
+		for lati in range(cls.MIN_LAT_DEG, cls.MAX_LAT_DEG, cls.HEIGHT_DEG):
+			for longi in range (cls.MIN_LONG_DEG, cls.MAX_LONG_DEG, cls.WIDTH_DEG):
+				cls.ZONES.append(Zone(Position(longi, lati), Position(longi+cls.WIDTH_DEG, lati+cls.HEIGHT_DEG)))
 
 #Initialisation des variables
 listAgents = []
 
+##############################################################""
 #Fonction main
 def main():
+	Zone.init_zones() #initialise les zones (attribut de classe : liste ZONES)
 	for dico in json.load(open("agents-100k.json")):
- 		pos = Position(dico.pop("longitude"),dico.pop("latitude"))
- 		listAgents.append(Agent(pos,**dico))
- 		
+		pos = Position(dico.pop("longitude"),dico.pop("latitude"))
+		listAgents.append(Agent(pos,**dico))
+	
 main()
+print(Zone.ZONES[50].coin1.lat_rad)
+	
 
-monAgent = listAgents[2]
-print(monAgent.position.latitude)
-print(monAgent.position.lat_rad)
