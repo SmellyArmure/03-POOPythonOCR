348dbd14937f28177029982e9ebd2957c66ca1f4
Maryse <muller.maryse@gmail.com>
2019-01-14 15:31:01 +0100

    crée une classe position, récupéré les positions de chaque agent, et affecté la position en attribut des agents ; crée des propriétés de la classe Position convertissant la latitude et la longitude en radians.

--- World.py
+++ World.py
@@ -1,21 +1,40 @@
 import json
+import math
 
 #Classe Agent
 class Agent:
-	def __init__(self, **agAttributs):
+	def __init__(self, position, **agAttributs):
 		for nomAtt, valAtt in agAttributs.items():
 			setattr(self, nomAtt, valAtt)
+			self.position = position
 
+#Classe Position
+class Position:
+	def __init__(self, longi, lati):
+		self.longitude = longi
+		self.latitude = lati
+
+	@property
+	def long_rad(self):
+		self._long_rad = self.longitude*math.pi/180
+		return self._lat_rad
+	
+	@property
+	def lat_rad(self):
+		self._lat_rad = self.latitude*math.pi/180
+		return self._lat_rad
+
+#Initialisation des variables
 listAgents = []
-listAgreeableness = []
 
+#Fonction main
 def main():
 	for dico in json.load(open("agents-100k.json")):
- 		listAgents.append(Agent(**dico))
- 		listAgreeableness.append(listAgents[-1].agreeableness)
-
+ 		pos = Position(dico.pop("longitude"),dico.pop("latitude"))
+ 		listAgents.append(Agent(pos,**dico))
+ 		
 main()
 
 monAgent = listAgents[2]
-print(str(monAgent.agreeableness))
-
+print(monAgent.position.latitude)
+print(monAgent.position.lat_rad)
